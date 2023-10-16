3c3c1fdedd59590bf7d6ea9328b8ebfa9a2d8d7f
Prica277 <prica277@hsd.k12.or.us>
2023-10-16 09:27:47 -0700

    beginning to add example dictionary

--- dictionary_project/cookie_dictionary.py
+++ dictionary_project/cookie_dictionary.py
@@ -3,3 +3,26 @@ Filename: cookie_dictionary.py
 Author: Alex Price
 Description: A set of dictionaries to be temporarily stored and placed properly into the code at a later time.
 """
+chocolate_chip = {
+    "prep_time": "5 minutes",
+    "cook_time": "12 minutes",
+    "chill_time": "30 minutes",
+    "total_time": "47 minutes",
+    "yield": "3 1/2 dozen",
+    "ingredients": {
+        "flour": "2 1/2 cups",
+        "milk_powder": "2 to 4 tbsp",
+        "salt": "1 tsp",
+        "baking_soda": "1 tsp",
+        "granulated_sugar": "¾ cup",
+        "brown_sugar": "¾ cup",
+        "butter": "1 cup",
+        "eggs": "2 large",
+        "vanilla_extract": "1 tsp",
+        "semi_sweet_chocolate_chips": "12 oz",
+    },
+    "step_list": {
+        
+    }
+}
+
