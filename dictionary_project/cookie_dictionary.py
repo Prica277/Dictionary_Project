4660a48ed4297a74213bdd75dd86645e61b970f8
Prica277 <prica277@hsd.k12.or.us>
2023-10-18 09:20:41 -0700

    adding second test dictionary

--- dictionary_project/cookie_dictionary.py
+++ dictionary_project/cookie_dictionary.py
@@ -22,7 +22,65 @@ chocolate_chip = {
         "semi_sweet_chocolate_chips": "12 oz",
     },
     "step_list": {
-        
+        "1": "In a medium mixing bowl, combine the flour, milk powder, salt, and baking soda and whisk to mix. Set aside.",
+	    "2": "With an electric mixer or in a stand mixer with a beater attachment, beat together the granulated sugar, brown sugar,and softened butter on medium speed until combined, 30 to 60 seconds.",
+	    "3": "Add the two eggs and vanilla to the butter-sugar mixture and beat at medium speed until no clumps or streaks remain, 10 to 20 seconds.",
+	    "4": "Add the flour mixture all at once. Beat on low speed until no more dry streaks of flour are visible, 20 to 40 seconds. You may still see some flecks of milk powder - this is fine.",
+	    "5": "Add all the chocolate chips and beat on low speed until the chocolate chips are evenly incorporated.",
+	    "6": "Cover the dough and refrigerate for at least 30 minutes orup to three days. Or freeze in individual scoops for up to three months.",
+        "7": "Heat the oven to 375 degrees Fahrenheit.",
+        "8": "Using a medium cookie scoop or a well-mounded tablespoon measure, scoop out the dough onto a baking sheet. Space the cookies about two inches apart.",
+	    "9": "Bake the cookies for 10 to 12 minutes. The cookies are done when they look slightly puffed in the center and are starting to turn toasty at the edges.",
+	    "10": "Cool the cookies on a baking sheet for about five minutes or until the puffed centers collapse and the cookies have firmed a little. Transfer the cookies to a wire rack and let cool completely.",
+        "11": "These cookies are at their best on the day they are made. Store the leftover cookies in an airtight container; they keep for about a week before becoming crumbly.",
+	},
+    "credits": {
+        "author_name": "Emma Christensen",
+        "site": "simplyrecipies.com",
+        "original_name": "Chewy Chocolate Chip Cookies",
     }
-}
+
+    }
+
+peanut_butter = {
+    "prep_time": "25 minutes",
+    "bake_time": "9-10 minutes",
+    "chill_time": "2 hours",
+    "total_time": "2 hours 35 minutes",
+    "yield": "30",
+    "ingredients": {
+        "flour": "1 1/4 cups",
+        "granulated_sugar": "1/2 cup",
+        "packed_brown_sugar": "1/2 cup",
+        "butter": "1/4 cup",
+        "eggs": "1",
+        "baking_soda": "3/4 tsp",
+        "baking_powder": "1/2 tsp",
+        "salt": "1/4 tsp",
+        "shortening": "1/4 cup",
+        "peanut_butter": "1/2 cup",
+    },
+    "step_list": {
+        "1": "Mix sugars, peanut butter, egg, shortening, and butter in a large bowl.",
+        "2": "Stir in remaining ingredients. You want the dough to stay light and fluffy so make sure not to overmix!",
+        "3": "Cover and refrigerate for about two hours until firm.",
+        "4": "Preheat oven to 375 degrees Fahrenheit.",
+        "5": "Shape dough into 1 1/4 inch balls.",
+        "6": "",
+        "7": "",
+        "8": "",
+        "9": "Cookies can be stored in an airtight container on the countertop for up to 5 days.",
+    },
+    "credits": {
+        "author_name": "Betty Crocker Kitchens",
+        "site": "bettycrocker.com",
+        "original_name": "Peanut Butter Cookies",
+    }
+
+
+    }
+
+
+
+
 
